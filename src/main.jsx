0adcc7f49e794b91f9976da556d0f03c9e471ab7
BrownGitHlungwani <blesson.nkateko@gmail.com>
2023-05-02 18:09:29 +0200

    created new fuction

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Display } from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Display title={"My App"} description={"Opher Ha Ha "}/>
   </React.StrictMode>,
 )
