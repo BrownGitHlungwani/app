80411ac51ec9154e48090cc7d44e183d43d59d49
BrownGitHlungwani <blesson.nkateko@gmail.com>
2023-05-06 17:49:30 +0200

    created barrel js file to export components to Navigation link JSX

--- src/components/Barrels/barrel.js
+++ src/components/Barrels/barrel.js
@@ -2,5 +2,7 @@ export * from '../index'
 export * from '../about'
 export * from '../blog'
 export * from '../products'
+export * from '../login'
+export * from '../signup'
 
 
--- src/components/NavigationComponents.jsx
+++ src/components/NavigationComponents.jsx
@@ -3,11 +3,15 @@ import { NavLink } from "react-router-dom";
 export function NavigationComponent(){
     return(
         <div className="NavBar">
+           <img className="" width={50} height={50} src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+
            <NavLink className="navlink" to="/">Home</NavLink>
            <NavLink className="navlink" to="/about">About</NavLink>
            <NavLink className="navlink" to="/blogs">Blogs</NavLink>
-           <NavLink className="navlink" to="/sale">Sales</NavLink>
+           <NavLink className="navlink" to="/sale">Special</NavLink>
            <NavLink className="navlink" to="/products">Products</NavLink>
+
+           <NavLink className="log" to="/login">Log in </NavLink>
         </div>   
     );
 }
