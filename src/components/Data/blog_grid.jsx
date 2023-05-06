a9886daf8eea8d0ca1b8df85aea83200a2f98f51
BrownGitHlungwani <blesson.nkateko@gmail.com>
2023-05-06 17:51:46 +0200

    created objects with image links and displayed in object jsx component using map.

---
+++ src/components/Data/blog_grid.jsx
@@ -0,0 +1,23 @@
+export const Blog_grid = [
+    {
+        name: "Motherboard",
+        description: "Get exclusive gaming boards with magnificent design and complex perfomance support.",
+        image: "https://c4.wallpaperflare.com/wallpaper/215/96/35/msi-motherboards-computer-technology-wallpaper-preview.jpg",
+        alt:"pic1",
+        agent: "Angel Holly",
+    },
+    {
+        name: "RAM",
+        description: "For up to 32 GB DDR4 flash speed RAM's, Designed for fast performance and gaming experties.",
+        image: "https://c4.wallpaperflare.com/wallpaper/860/946/365/pc-gaming-motherboards-msi-computer-wallpaper-preview.jpg",
+        alt:"pic2",
+        agent: "Dr Brown H",
+    },
+    {
+        name: "CPU",
+        description: "Acknowledge the Quad-core vs Dual-core processor performance and functionality. ",
+        image: "https://c4.wallpaperflare.com/wallpaper/25/377/176/microchip-cpu-robot-leds-wallpaper-preview.jpg",
+        alt:"pic3",
+        agent: "Professor Intel",
+    },
+]
--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,8 +1,22 @@
+import { Blog_grid } from "./Data/blog_grid";
+
 export function Blog() {
+
     return(
-        <>
-         <h1>My Blog</h1>
-         <p>Really</p>
-        </>
+        <div className="Blogbody">
+           <h1>From the blog</h1>
+           <h4>Learn about different computer parts and use case.</h4>
+           <div className="Blog_gridholder">
+              {
+                Blog_grid.map(item =>(
+                <div>
+                    <h2>{item.name}</h2>
+                    <img src={item.image} alt={item.alt} width={410} height={250} />
+                    <h3>{item.agent}</h3>
+                    <p>{item.description}</p>
+                </div>
+              ))}
+            </div>
+        </div>
     );
 }
